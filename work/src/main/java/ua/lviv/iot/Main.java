42e6c764f8a68694755002def6b6efc1dc1fed65
YazHek <kruchookjarema@gmail.com>
2019-05-10 19:01:03 +0300

    Commited a seven java lab

---
+++ work/src/main/java/ua/lviv/iot/Main.java
@@ -0,0 +1,17 @@
+package ua.lviv.iot;
+
+import java.io.IOException;
+
+public class Main {
+
+
+    public static void main(String...args) throws IOException {
+
+        StringProcessor stringProcessor = new StringProcessor();
+        stringProcessor.readInputText();
+        String test = stringProcessor.formattingText();
+        System.out.print(test);
+
+
+    }
+}
---
+++ work/src/main/java/ua/lviv/iot/StringProcessor.java
@@ -0,0 +1,55 @@
+package ua.lviv.iot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringProcessor {
+
+    private String text;
+    private int num;
+
+
+    public StringProcessor() {
+
+    }
+
+    public StringProcessor(final String textObj, final int numObj) {
+
+        this.text = textObj;
+        this.num = numObj;
+    }
+
+    public final void readInputText() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                System.in, StandardCharsets.UTF_8));
+        System.out.print("Write your string: ");
+        this.text = br.readLine();
+        System.out.print("Write your number");
+        this.num = Integer.parseInt(br.readLine());
+    }
+
+    public final String formattingText() {
+
+        StringBuffer sBuffer = new StringBuffer();
+        String[] sentences = text.split("\\\\s+");
+        String regex = String.format("\\\\d{%d}(.docx|.doc|.txt|.odb)$", num);
+        Pattern pattern = Pattern.compile(regex);
+
+        for (String sentence : sentences) {
+
+            Matcher matcher = pattern.matcher(sentence);
+            if (!matcher.find()) {
+
+                sBuffer.append(sentence);
+                sBuffer.append(" ");
+            }
+        }
+
+        return sBuffer.toString().trim();
+    }
+}
